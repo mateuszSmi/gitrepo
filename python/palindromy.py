acc4ca9ac95d66d8f1e1120e2a69a9cd0f6b6b6b
mateuszSmi <mateusz.smigala@lo1.sandomierz.pl>
2019-03-05 13:31:27 +0100

    slownik

--- python/palindromy.py
+++ python/palindromy.py
@@ -3,40 +3,45 @@
 #
 #  palindromy.py
 
-import os
 
-def czy_palindrom(tekst):
-    ile = len(napis)
+
+import os 
+
+def czy_palidrom(tekst):
+    ile = len(tekst)
     for i in range(ile // 2):
         if tekst[i] != tekst[-i-1]:
             return False
+
     return True
-
-
+    
 def czytaj_dane(plik):
-    if not os.path_exists(plik):
-        print('plik niedostepny')
+    if not os.path.exists(plik):
+        print('Plik niedostępny')
         return False
-        
     teksty = []
     with open(plik, "r") as f:
         for linia in f:
             teksty.append(linia.strip())
-    return teksty
-    
-    
-
+        return teksty
 
 def main(args):
-    #tekst = input("Podaj tekst:")
+    # tekst = input("podaj tekst ")
     teksty = czytaj_dane('dane01.txt')
     ile = 0
-    # ~if czy_palindrom(tekst):
-        # ~print('To palindrom!')
+    for i in range (len(teksty)):
+        if czy_palidrom(teksty[i]):
+            ile += 1
+
+        else:
+            continue
+    # ~if czy_palidrom(tekst):
+        # ~print('to palindrom')
     # ~else:
-        # ~print('To nie palindrom!')
-    print(teksty)
+        # ~print(' to nie jest palindrom')
+    print (ile)
     return 0
+
 
 if __name__ == '__main__':
     import sys
