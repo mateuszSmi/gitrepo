9ab3bfdaae5ac2b192a3ba8bf52ccc8324036840
mateuszSmi <mateusz.smigala@lo1.sandomierz.pl>
2019-09-30 08:59:35 +0200

    elo

---
+++ cpp/petlecw4.cpp
@@ -0,0 +1,20 @@
+/*
+ * bez nazwy.cxx
+ * 
+ * Copyright 2019  <>
+ */
+
+
+#include <iostream>
+using namespace std;
+int main(int argc, char **argv)
+{
+    int i=10;
+    for(i=10;i<=100;i+=2){
+    if(i%3==0){
+        cout << i <<endl;
+    }
+    }
+	return 0;
+}
+
