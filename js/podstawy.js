7b00698948da82b3f785a97accc1b6cf40079426
mateuszSmi <mateusz.smigala@lo1.sandomierz.pl>
2020-01-17 10:36:48 +0100

    sdasdas

---
+++ js/podstawy.js
@@ -0,0 +1,14 @@
+<script>
+// Edit your script here
+var dlugosc =10;        //liczby
+var imie ="Dorota";        //string
+var osoba ={
+imie:"Adam",
+nazwisko:"Słodowski",
+wiek:25
+};
+document.write(osoba.imie, osoba.wiek);
+var tablica= ["Fiat ", "skoda","mercedes"];
+document.write(tablica[1]);
+</script>
+<!-- edit your html here -->
---
+++ js/trojkat.js
@@ -0,0 +1,13 @@
+<script>
+// Edit your script here
+var a = 5;
+var b = 10;
+var c = a + b;
+console.log(c);
+document.write("<h1>"+c+"</h1>")
+window.alert(c);
+document.write('<p id="js"></p>');
+document.getElementById("js").innerHTML=c;
+
+</script>
+<!-- edit your html here -->
