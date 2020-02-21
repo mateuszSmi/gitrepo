6ab24e6646d3483ba51651ddb9f78578ac3e1402
mateuszSmi <mateusz.smigala@lo1.sandomierz.pl>
2020-02-21 10:35:09 +0100

    dssda

--- js/jquert_cwicz.js
+++ js/jquert_cwicz.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
   $(".wynik").text("Nowa treść");
   console.log($("p").text());
-    // wywołania jQuery
+  $("#przycisk").click(function(){
+    var vnk = parseInt($("#vnk").val());
+    var nk = parseInt($("#nk").val());
+    var R = vnk / nk * 100;
+    var div2 = '<div class="wspolczynnik">'+R+'</div>';
+    $("div.wynik").after(div2);
+  });
+  $("input").mouseover(function(){
+    $(this).addClass("tloBlue");
+  });
 });
