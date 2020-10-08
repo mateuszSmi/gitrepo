ce680f20f94a10370735ab5da01f17d8e1eba5cf
mateuszSmi <mateusz.smigala@lo1.sandomierz.pl>
2020-10-08 12:50:09 +0200

    migawak

--- tbUczniowie.sql
+++ tbUczniowie.sql
@@ -1,17 +1,22 @@
+CREATE TABLE tbOddzialy (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    Oznaczenie CHAR(3),
+    RokNaboru CHAR(4),
+    RokMatury CHAR(4)
+    
+);
+
 CREATE TABLE tbUczniowie (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     imie TEXT,
+    nazwisko TEXT,
     plec INTEGER,
-    data_ur DATA,
-    awatar BLOB,
+    data_ur DATE,
+    awatar blob,
     egzamin INTEGER,
     srednia_ocen DECIMAL,
     uwagi TEXT,
-);
-
-CREATE TABLE tbOddzialy(
-    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    Oznaczenie TEXT,
-    RokNaboru TEXT,
-    RokMatury TEXT
+    
+    klasa_id INTEGER,
+    FORGEIN KEY (klasa_id) REFERENCES tbOddzialy (id)
 );
